cbafdaa58787a8282ba3c9b910e975d51c46358c
vattcarter7 <vattcarter@gmail.com>
2021-09-05 04:38:32 +0700

    remove react logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -10,14 +9,13 @@ function App() {
     <Router>
       <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
           <a
             className='App-link'
             href='https://reactjs.org'
             target='_blank'
             rel='noopener noreferrer'
           >
-            Learn React With Kubernetes
+            Learn React With Kubernetes !!!
           </a>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
