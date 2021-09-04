44f9ee994f3c789b109aa4fb8fe7075679112164
vattcarter7 <vattcarter@gmail.com>
2021-09-04 18:26:59 +0700

    update homepage

--- client/src/App.js
+++ client/src/App.js
@@ -8,23 +8,23 @@ import Fib from './Fib';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Learn React 3
+            Learn React With Kubernetes
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to='/'>Home</Link>
+          <Link to='/otherpage'>Other Page</Link>
         </header>
         <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+          <Route exact path='/' component={Fib} />
+          <Route path='/otherpage' component={OtherPage} />
         </div>
       </div>
     </Router>
